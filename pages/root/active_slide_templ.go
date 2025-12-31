460cdf1338a90dab67073e74144a435d59f965db
Gerhard Just-Olsen <cinmay05@gmail.com>
2025-12-31 20:11:17 +0100

    Added who slides

--- pages/root/active_slide_templ.go
+++ pages/root/active_slide_templ.go
@@ -23,6 +23,9 @@ var deck = []slideFunc{
 	func() templ.Component { return slides.LobbyWelcome(isPresenterMode) },
 	func() templ.Component { return slides.WhyThisTalk(isPresenterMode) },
 	func() templ.Component { return slides.Agenda(slides.AgendaWhoWeAre, isPresenterMode) },
+	func() templ.Component { return slides.WhatIsRegncon(isPresenterMode) },
+	func() templ.Component { return slides.WhoWeAre(isPresenterMode) },
+	func() templ.Component { return slides.DifferentViewpoints(isPresenterMode) },
 	func() templ.Component { return slides.Agenda(slides.Agenda2024, isPresenterMode) },
 	func() templ.Component { return slides.Agenda(slides.Agenda2025, isPresenterMode) },
 	func() templ.Component { return slides.Agenda(slides.AgendaCompare, isPresenterMode) },
@@ -85,7 +88,7 @@ func ActiveSlide(db *sql.DB) templ.Component {
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(fmt.Sprintf("{ slideIndex: %d }", index))
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/root/active_slide.templ`, Line: 53, Col: 61}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `pages/root/active_slide.templ`, Line: 56, Col: 61}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
